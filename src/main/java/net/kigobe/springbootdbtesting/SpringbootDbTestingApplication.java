60d41d05f65ca9650511f9799f135b0a0410bfb5
rkigobe <rkigobe@mathworks.com>
2024-05-22 10:45:08 -0400

    first commit

---
+++ src/main/java/net/kigobe/springbootdbtesting/SpringbootDbTestingApplication.java
@@ -0,0 +1,13 @@
+package net.kigobe.springbootdbtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDbTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDbTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kigobe/springbootdbtesting/SpringbootDbTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kigobe.springbootdbtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDbTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
